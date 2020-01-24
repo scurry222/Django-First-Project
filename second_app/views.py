30a30de4870ad323e86c4e9afc6275ed38b74757
scurry222 <scurry222@gmail.com>
2020-01-23 20:11:43 -0800

    inject html templates through second_app

--- second_app/views.py
+++ second_app/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<em>My Second App</em>")
+    my_dict = {'insert_me': "I am from views.py"}
+    return render(request, 'second_app/index.html', context=my_dict)
