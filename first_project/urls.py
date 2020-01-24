5deb85921762864a7559b3157218cedc54bb5318
scurry222 <scurry222@gmail.com>
2020-01-23 18:54:02 -0800

    url mapping second app

--- first_project/urls.py
+++ first_project/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.urls import path
 # from first_app import views
@@ -21,5 +21,6 @@ from second_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^second_app/', include('second_app.urls')),
     path('admin/', admin.site.urls),
 ]
