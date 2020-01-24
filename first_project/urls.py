5f6a243a424ee80072d7a1181ed7ba83367809d7
scurry222 <scurry222@gmail.com>
2020-01-23 18:17:09 -0800

    second app- imbedded html tag

--- first_project/urls.py
+++ first_project/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
-from first_app import views
+# from first_app import views
+from second_app import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
