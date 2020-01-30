3af701c4d9e3a68a95aca170a76f821c1ed2b5da
scurry222 <scurry222@gmail.com>
2020-01-30 13:34:30 -0800

    check for direction in html attributes

--- second_app/views.py
+++ second_app/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': webpages_list}
     my_dict = {'insert_me': "I am from views.py"}
     return render(request, 'second_app/index.html', context=my_dict)
 
